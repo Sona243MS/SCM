d2bbee72b20a9e40323dc7431d0c9de5330b76ca
Sona243MS <sona.simmy@s.amity.edu>
2025-03-05 14:58:46 +0530

    Added a case in test branch

---
+++ pattern.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int i,a,b;
+	printf("Enter the choice or case number");
+	scanf("%d",&i);
+	printf("Enter a");
+	scanf("%d",&a);
+	printf("Enter b");
+	scanf("%d",&b);
