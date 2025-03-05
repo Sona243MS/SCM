8db6a39dba25901e8ae22c4eb037f03cb447882d
Sona243MS <sona.simmy@s.amity.edu>
2025-03-05 13:34:47 +0530

    Negative Number code added

---
+++ pos.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("Enter a number");
+	scanf("%d",&num);
+	if(num>0)
+	{
+		printf("Positive number %d",num);
+	}
+	else if(num<0)
+	{
+		printf("Negative number %d",num);
+	return 0;
+
+}
