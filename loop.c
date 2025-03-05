ba9026e2c82de53e5b8d84b9beb220f2f1031974
Sona243MS <sona.simmy@s.amity.edu>
2025-03-05 11:35:12 +0530

    Added a description

--- loop.c
+++ loop.c
@@ -4,13 +4,17 @@ int main()
 	int num;
 	printf("Enter a number");
 	scanf("%d",&num);
+	printf('Check if numbers from 1 to 10 is even or odd");
+	for(int i=1;i<=10;i++)
+	{
 	if(num%2==0)
 	{
-		printf("%d is even number",num);
+		printf("%d Even Number",num);
 	}
 	else
 	{
-		prinf("%d is odd number",num);
+		prinf("%d Odd Number",num);
+	}
 	}
 	return 0;
 }
