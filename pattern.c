c4f03c6f587a2d7f57554b3bb7dc941fa715f15f
Sona243MS <sona.simmy@s.amity.edu>
2025-03-07 10:37:21 +0530

    Added an array to second case

--- pattern.c
+++ pattern.c
@@ -23,5 +23,30 @@ printf("%d is maximum",a);
 break;
 }
 	case 2:
-    {
+    {int i,j,r,c,value,sum=0,mult=1;
+	    printf("Case for Matrix\n");
+	    printf("Enter the number of rows");
+	    scanf("%d",&r);
+	    printf("Enter the number of columns");
+	    scanf("%d",&c);
+	    int a[r][c];
+	    for(i=0;i<r;i++)
+	    {
+		    for(j=0;j<c;j++)
+		    {
+		 printf("Enter a[%d][%d]=",i,j);
+	    scanf("%d",&value);
+			    a[i][j]=value;
+			    sum+=a[i][j];
+			    mult*=a[i][j];
+		    }
+	    }
+	    printf("Sum=%d \n",sum);
+	    printf("Product=%d",mult);
+    }
+break;
+}
+return 0;
+}
+
 
