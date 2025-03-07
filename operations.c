7e5320c8616130b47aa94e8c7a049abcd99e5cc2
Sona243MS <sona.simmy@s.amity.edu>
2025-03-07 06:47:19 +0530

    M ade changes to make the values users choice

--- operations.c
+++ operations.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main()
 {
-	int a=40,b=20;
-	int add=a+b;
+	int a,b,add,mult,div;
+	printf("Enter first number ");
+	scanf("%d",&a);
+	printf("Enter second number");
+	scanf("%d",&b);
+	add=a+b;
 	printf("Sum= %d",add);
-	int sub=a-b;
+	sub=a-b;
 	printf("Subtract= %d",sub);
-	int mul=a*b;
+	mul=a*b;
 	printf("Multiplication= %d",mul);
-	int div=a/b;
-	printf("Division= %d",div);
+	div=a/b;
+        printf("Division= %d",div);
 	return 0;
 }
