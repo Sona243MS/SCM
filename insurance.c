607539774222bf76d90ab34b713d02e2fe22e417
Sona243MS <sona.simmy@s.amity.edu>
2025-03-13 14:28:00 +0530

    Added basic info required about a person

---
+++ insurance.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char ge,ms;
+	int age;
+	printf("Enter Marital status(M/N) :\n");
+	scand("%c",&ms);
+	printf("Enter gender(M/F) :\n");
+	scanf("%c",&ge);
+	printf("Enter age:\n");
+	scanf("%d",&age);
+
