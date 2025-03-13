e8bdca25b253a15ac1f337bfacc1f357049f5562
Sona243MS <sona.simmy@s.amity.edu>
2025-03-13 11:28:47 +0530

    Added logic for conversions to feet,inch,cm,m

---
+++ convert.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int dis,m,cm,feet,inch;
+	printf("Enter distance between two places in km");
+	scanf("%d",&dis);
+	m=dis*1000;
+	cm=dis*100000;
+	feet=dis*3280;
+	inch=dis*39370;
+	
