1874457704ac81bd82d07f5a359fe71d1da273cf
Sona243MS <sona.simmy@s.amity.edu>
2025-03-21 06:41:36 +0530

    Added the body of the loop

--- fibonacci.c
+++ fibonacci.c
@@ -4,8 +4,15 @@ int main()
 	int num,1=0,num2=1;n,t;
 	printf("Enter n=");
 	scanf("%d",&n);
-	printf("Fibonacci Series= %d %d",num1,num2);
+	printf("Fibonacci Series= %d %d ",num1,num2);
 	for(int i=o;i<=n;i++)
 	{
 		t=num1+num2;
 		num1=num2;
+		num2=t;
+		printf("%d ",t);
+	}
+	return 0;
+}
+
+
