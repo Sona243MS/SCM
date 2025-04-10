76b950f7c8502749cc1132e41eca90b6cd9b1633
Sona243MS <sona.simmy@s.amity.edu>
2025-04-10 18:14:12 +0530

    Added bubble sort code

--- sort2D.c
+++ sort2D.c
@@ -8,3 +8,16 @@ void sort2DArray(int rows, int cols, int arr[rows][cols]) {
     // Treat the 2D array as 1D array for sorting
     int *ptr = &arr[0][0];
 
+    // Bubble sort algorithm
+    for (int i = 0; i < totalElements - 1; i++) {
+        for (int j = 0; j < totalElements - i - 1; j++) {
+            if (ptr[j] > ptr[j + 1]) {
+                // Swap elements
+                int temp = ptr[j];
+                ptr[j] = ptr[j + 1];
+                ptr[j + 1] = temp;
+            }
+        }
+    }
+}
+
