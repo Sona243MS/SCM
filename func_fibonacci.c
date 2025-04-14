1e3f30eca009760081c036149ffdeded1cc25ccc
Sona243MS <sona.simmy@s.amity.edu>
2025-04-14 08:17:38 +0530

    Added comments to the code

--- func_fibonacci.c
+++ func_fibonacci.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+//fibonacci() to print the fibonacci series upto n
 int fibonacci()
 {
 	int a=0,b=1,c,n=10;
@@ -6,13 +7,14 @@ int fibonacci()
 	for(int i=0;i<n;i++)
 	{
 		c=a+b;
+		//swapping the values
 		a=b;
 		b=c;
 		printf("%d ",c);
 	}
 	return 0;
 }
-
+//main function 
 int main()
 {
 	int n;
